96513cd762779b1c3267175ad61a00ef415f1081
gauravptil <gaurav.p@orangewood.co>
2018-05-29 14:50:28 +0530

    Version 1.0

---
+++ README.txt
@@ -0,0 +1,3 @@
+Communication between RPI and ESP32 with the help of router
+
+mainCode: code to be uploaded on esp32. it will connect you on wifi and assign you IP, then you can use this ip to connect from RPI
---
+++ mainCode/mainCode.ino
@@ -0,0 +1,60 @@
+#include <ETH.h>
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiClient.h>
+#include <WiFiGeneric.h>
+#include <WiFiMulti.h>
+#include <WiFiScan.h>
+#include <WiFiServer.h>
+#include <WiFiSTA.h>
+#include <WiFiType.h>
+#include <WiFiUdp.h>
+
+
+WiFiMulti WiFiMulti;
+WiFiServer server(80);
+
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+  WiFiMulti.addAP("orangewoodlabs", "9953562788");
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Wait for WiFi... ");
+
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+  delay(500);
+}
+int value = 0;
+
+void loop() {
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        }
+     }
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
